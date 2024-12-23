3351df509f52fb8e5c01235959757a1278282140
KerrieMeister96 <koensmit13@gmail.com>
2024-12-23 01:38:52 +0100

    basic page setup

---
+++ Robot-App/Components/Pages/Robot.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Robot_App.Components.Pages
+{
+    public partial class Robot : ComponentBase
+    {
+        public string Name { get; set; } = "Koen Smit";
+    }
+}
