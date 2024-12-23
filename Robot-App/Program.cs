d6ae16c72655ab95a293e4f070ace7630de5b30c
KerrieMeister96 <koensmit13@gmail.com>
2024-12-23 02:15:45 +0100

    program changed

--- Robot-App/Program.cs
+++ Robot-App/Program.cs
@@ -1,5 +1,5 @@
 using Robot_App.Components;
-using Robot_App.Services;
+using Robot_App.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,34 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// Register MQTT Service as a Singleton
-builder.Services.AddSingleton<MqttService>();
+builder.Services.AddSingleton<IUserRepository, SqlUserRepository>(sp => new SqlUserRepository("Server=localhost;Database=RobotApp;User Id=sa;Password=Password123!"));
 
 var app = builder.Build();
-
-// Configure the MQTT service
-// Start the MQTT service asynchronously in the background
-var mqttService = app.Services.GetRequiredService<MqttService>();
-_ = Task.Run(async () =>
-{
-    await mqttService.ConnectAsync(
-        // host: "your_mqtt_host",
-        // port: 8883,
-        // clientId: "webapp",
-        // username: "your_username",
-        // password: "your_password"
-    );
-});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
