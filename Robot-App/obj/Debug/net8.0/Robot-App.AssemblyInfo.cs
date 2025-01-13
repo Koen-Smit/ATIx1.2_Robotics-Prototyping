6bd212e73734096b4c29a8be7aebf6dacb12e3f0
KerrieMeister96 <koensmit13@gmail.com>
2025-01-13 02:22:48 +0100

    ux/ui changed

--- Robot-App/obj/Debug/net8.0/Robot-App.AssemblyInfo.cs
+++ Robot-App/obj/Debug/net8.0/Robot-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Robot-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1582e221563ff0cf7172a34f54f28f8d66e1489")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ce59241adec95234a5965acd94cd5aca33a675")]
 [assembly: System.Reflection.AssemblyProductAttribute("Robot-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Robot-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
